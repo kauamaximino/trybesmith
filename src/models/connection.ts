e5f220832ac67afd1d531c8e654dd6fd5b3263cc
Kauã Maximino <kaua_maximino@outlook.com>
2022-07-29 15:10:43 -0300

    chore: configurações inicias

--- src/models/connection.ts
+++ src/models/connection.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
 });
